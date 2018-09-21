f2fe79b806a2e42df09f40e4e04fc1b7578ff100
levonman <levonmanukyan91@gmail.com>
2018-09-21 13:08:38 +0400

    backend

--- backend/views/menu/_form.php
+++ backend/views/menu/_form.php
@@ -13,25 +13,43 @@ use dosamigos\ckeditor\CKEditor;
 <div class="menu-form">
 
     <?php $form = ActiveForm::begin(); ?>
+    <div class="">
+        <ul id="myTab" class="nav nav-tabs" style="margin-left: -1px;">
+            <li class="<?= Yii::$app->language == 'am' ? 'active' : '' ?>"><a href="#am"
+                                                                              data-toggle="tab"><?= Yii::t('app', 'Armenian'); ?></a>
+            </li>
+            <li class="<?= Yii::$app->language == 'ru' ? 'active' : '' ?>"><a href="#ru"
+                                                                              data-toggle="tab"><?= Yii::t('app', 'Russian'); ?></a>
+            </li>
+            <li class="<?= Yii::$app->language == 'en' ? 'active' : '' ?>"><a href="#en"
+                                                                              data-toggle="tab"><?= Yii::t('app', 'English'); ?></a>
+            </li>
+        </ul>
+        <div class="tab-content">
+            <div class="tab-pane fade active in" id="am">
+                <?= $form->field($model, 'name_am')->textInput(['maxlength' => true]) ?>
+            </div>
+            <div class="tab-pane fade" id="ru">
+                <?= $form->field($model, 'name_ru')->textInput(['maxlength' => true]) ?>
+            </div>
+            <div class="tab-pane fade" id="en">
+                <?= $form->field($model, 'name_en')->textInput(['maxlength' => true]) ?>
+            </div>
+        </div>
 
-    <?= $form->field($model, 'name_am')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'name_en')->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'type')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'name_ru')->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'type_id')->textInput() ?>
 
-    <?= $form->field($model, 'type')->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'url')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'type_id')->textInput() ?>
+        <?= $form->field($model, 'position')->dropDownList(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'url')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'position')->dropDownList(['maxlength' => true]) ?>
-
-    <div class="form-group">
-        <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
+        <div class="form-group">
+            <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
+        </div>
     </div>
-
     <?php ActiveForm::end(); ?>
 
 </div>
--- backend/views/post/_form.php
+++ backend/views/post/_form.php
@@ -2,7 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-use mihaildev\ckeditor\CKEditor;
+use yii\redactor\widgets\Redactor;
 
 $this->registerJsFile(
     '/mb_admin/js/modal.js',
@@ -17,55 +17,52 @@ $this->registerJsFile(
 <div class="post-form">
 
     <?php $form = ActiveForm::begin(); ?>
-
-    <?= $form->field($model, 'title_am')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'title_ru')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'title_en')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'caption_am')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'caption_ru')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'caption_en')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'content_am')->textarea(['rows' => 6])->widget(CKEditor::className(),[
-        'editorOptions' => [
-            'preset' => 'basic', //разработанны стандартные настройки basic, standard, full данную возможность не обязательно использовать
-            'inline' => false, //по умолчанию false
-        ],
-    ]); ?>
-
-    <?= $form->field($model, 'content_ru')->textarea(['rows' => 6])->widget(CKEditor::className(),[
-        'editorOptions' => [
-            'preset' => 'basic', //разработанны стандартные настройки basic, standard, full данную возможность не обязательно использовать
-            'inline' => false, //по умолчанию false
-        ],
-    ]); ?>
-
-    <?= $form->field($model, 'content_en')->textarea(['rows' => 6])->widget(CKEditor::className(),[
-        'editorOptions' => [
-            'preset' => 'basic', //разработанны стандартные настройки basic, standard, full данную возможность не обязательно использовать
-            'inline' => false, //по умолчанию false
-        ],
-    ]); ?>
-
-    <?= $form->field($model, 'picture')->hiddenInput()->label(false) ?>
+    <div class="">
+        <ul id="myTab" class="nav nav-tabs" style="margin-left: -1px;">
+            <li class="<?= Yii::$app->language == 'am' ? 'active' : '' ?>"><a href="#am"
+                                                                              data-toggle="tab"><?= Yii::t('app', 'Armenian'); ?></a>
+            </li>
+            <li class="<?= Yii::$app->language == 'ru' ? 'active' : '' ?>"><a href="#ru"
+                                                                              data-toggle="tab"><?= Yii::t('app', 'Russian'); ?></a>
+            </li>
+            <li class="<?= Yii::$app->language == 'en' ? 'active' : '' ?>"><a href="#en"
+                                                                              data-toggle="tab"><?= Yii::t('app', 'English'); ?></a>
+            </li>
+        </ul>
+        <div class="tab-content">
+            <div class="tab-pane fade active in" id="am">
+                <?= $form->field($model, 'title_am')->textInput(['maxlength' => true]) ?>
+                <?= $form->field($model, 'caption_am')->textInput(['maxlength' => true]) ?>
+                <?= $form->field($model, 'content_am')->widget(Redactor::className()); ?>
+            </div>
+            <div class="tab-pane fade" id="ru">
+                <?= $form->field($model, 'title_ru')->textInput(['maxlength' => true]) ?>
+                <?= $form->field($model, 'caption_ru')->textInput(['maxlength' => true]) ?>
+                <?= $form->field($model, 'content_ru')->widget(Redactor::className()); ?>
+            </div>
+            <div class="tab-pane fade" id="en">
+                <?= $form->field($model, 'title_en')->textInput(['maxlength' => true]) ?>
+                <?= $form->field($model, 'caption_en')->textInput(['maxlength' => true]) ?>
+                <?= $form->field($model, 'content_en')->widget(Redactor::className()); ?>
+            </div>
+        </div>
 
 
-    <?= $form->field($model, 'type')->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'picture')->hiddenInput()->label(false) ?>
 
 
-<!------------------------------------- Modal -------------------------------------------------------->
-    <?= $this->render('_modal', [
-        'media' => $media,
-    ]) ?>
-<! ------------------------------------------------------------------------ !>
-    <div class="form-group">
-        <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
+        <?= $form->field($model, 'type')->textInput(['maxlength' => true]) ?>
+
+
+        <!------------------------------------- Modal -------------------------------------------------------->
+        <?= $this->render('_modal', [
+            'media' => $media,
+        ]) ?>
+        <! ------------------------------------------------------------------------ !>
+        <div class="form-group">
+            <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
+        </div>
     </div>
-
     <?php ActiveForm::end(); ?>
 
 </div>
